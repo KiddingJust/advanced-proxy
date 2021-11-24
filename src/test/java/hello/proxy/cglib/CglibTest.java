eecc4312cc688e241e745f4687ff71da4444c28a
Kidding Just <rlarkdlr@gmail.com>
2021-11-24 21:41:13 +0900

    advanced proxy

---
+++ src/test/java/hello/proxy/cglib/CglibTest.java
@@ -0,0 +1,27 @@
+package hello.proxy.cglib;
+
+import hello.proxy.cglib.code.TimeMethodInterceptor;
+import hello.proxy.common.service.ConcreteService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+
+    @Test
+    void cglib(){
+        ConcreteService target = new ConcreteService();
+
+        Enhancer enhancer = new Enhancer();
+        //동적 프록시를 만드는데, 인터페이스를 지정하는 게 아니라 상속받은 구체 클래스로...
+        enhancer.setSuperclass(ConcreteService.class);
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        ConcreteService proxy = (ConcreteService) enhancer.create();
+
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+
+        proxy.call();
+    }
+}
